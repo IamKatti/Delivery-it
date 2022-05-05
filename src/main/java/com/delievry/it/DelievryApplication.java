595b966ec3c90e1c4853c1ccd009083d3bcc2100
unknown <amareshmankatti@gmail.com>
2022-05-05 20:05:09 +0530

    First commit

---
+++ src/main/java/com/delievry/it/DelievryApplication.java
@@ -0,0 +1,13 @@
+package com.delievry.it;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelievryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DelievryApplication.class, args);
+	}
+
+}
